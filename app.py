c97401e30e0bba6bbee770584aac4708982d6179
imfulee <freems5192@gmail.com>
2020-09-15 16:59:27 +0800

    Initialize race.html and route it

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import json
 from werkzeug.utils import secure_filename
 from google.cloud import texttospeech
 
+import route
+
 app=Flask(__name__)
 bootstrap=Bootstrap(app)
 
@@ -28,6 +30,10 @@ def OTA():
 @app.route('/OTS')
 def OTS():
 	return render_template('OWLToSpeech.html')
+
+@app.route('/race')
+def race():
+	return render_template('race.html')
 
 @app.route('/savetxt', methods=['POST'])
 def savetxt():
@@ -77,7 +83,6 @@ def TTSapi():
 		applic(message)
 		return jsonify({'result': True})
 	return jsonify({'result': False})
-
 
 def owlverb(storypath):
 	result=subprocess.run(['owl_to_ace.exe','-xml',f'upload/{storypath}'],capture_output=True,shell=True)
