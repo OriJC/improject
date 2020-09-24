8b95b0affaec17236d027439bf23f130f7cae871
imfulee <freems5192@gmail.com>
2020-09-25 01:34:02 +0800

    Didn't finish fixing merge conflict of app.py on previous commit

--- app.py
+++ app.py
@@ -79,7 +79,6 @@ def savetxt():
 
 @app.route('/TTSapi', methods=['POST'])
 def TTSapi():
-<<<<<<< HEAD
 	if request.method == 'POST':
 		text=request.values['text']
 		filename=request.values['filename'];
@@ -115,42 +114,6 @@ def TTSapi():
 		applic(message,filename)
 		return jsonify({'result': True,'filename':filename})
 	return jsonify({'result': False})
-=======
-    if request.method == 'POST':
-        text = request.values['text']
-        pitch = 0
-        speed = 1
-        lang = 'en-US'
-        if(request.values['voice']) == 'male1':
-            name = "en-US-Wavenet-D"
-            gender = texttospeech.SsmlVoiceGender.MALE
-        elif(request.values['voice'] == 'female1'):
-            name = "en-US-Wavenet-F"
-            gender = texttospeech.SsmlVoiceGender.FEMALE
-        elif(request.values['voice'] == 'male2'):
-            name = "en-US-Wavenet-A"
-            gender = texttospeech.SsmlVoiceGender.MALE
-        elif(request.values['voice'] == 'female2'):
-            name = "en-US-Wavenet-E"
-            gender = texttospeech.SsmlVoiceGender.FEMALE
-        elif(request.values['voice'] == 'robot'):
-            name = "en-GB-standard-A"
-            gender = texttospeech.SsmlVoiceGender.MALE
-            speed = 0.66
-            pitch = -10.40
-            lang = 'en-GB'
-        message = json.dumps({
-            'lang': lang,
-            'inputtext': text,
-            'name': name,
-            'gender': gender,
-            'speed': speed,
-            'pitch': pitch
-        })
-        applic(message)
-        return jsonify({'result': True})
-    return jsonify({'result': False})
->>>>>>> ae9063a0364309941449906e8e62c13509ce6293
 
 def owlverb(storypath):
     result = subprocess.run(
@@ -158,7 +121,6 @@ def owlverb(storypath):
     return result.stdout.decode()
 
 
-<<<<<<< HEAD
 def applic(message,filename):
 	client = texttospeech.TextToSpeechClient()
 	message=json.loads(message)
@@ -184,26 +146,3 @@ def deleteText():
 
 if __name__=='__main__':
 	app.run(debug=True)
-=======
-def applic(message):
-    client = texttospeech.TextToSpeechClient()
-    message = json.loads(message)
-    synthesis_input = texttospeech.SynthesisInput(text=message['inputtext'])
-    voice = texttospeech.VoiceSelectionParams(
-        language_code=message['lang'],
-        ssml_gender=message['gender'],
-        name=message['name'])
-    audio_config = texttospeech.AudioConfig(
-        speaking_rate=message['speed'],
-        pitch=message['pitch'],
-        audio_encoding=texttospeech.AudioEncoding.MP3)
-    response = client.synthesize_speech(
-        input=synthesis_input, voice=voice, audio_config=audio_config)
-    with open('./static/output.mp3', 'wb') as out:
-        out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
-    return send_file('./static/output.mp3', attachment_filename='output.mp3')
-
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> ae9063a0364309941449906e8e62c13509ce6293
