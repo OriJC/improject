6ee93212a5856715d58644939ee97242052a46b4
imfulee <imfulee@gmail.com>
2020-10-30 01:50:00 +0800

    remove uneccesary comments

--- static/js/send_tts.js
+++ static/js/send_tts.js
@@ -1,6 +1,5 @@
 function send_tts() {
     /* send the data from conclusion to OwlToSpeech.html */
-    // TODO: make sure a way to catch if there is no conclusion node
     var send_file = new FormData();
     var conclusion_data = "";
 
@@ -17,7 +16,5 @@ function send_tts() {
         data: send_file,
         processData: false,
         contentType: false,
-    }).done(function () {
-        // location.href = "/TTS";
-    });
+    })
 }
