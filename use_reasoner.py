ad279a6a21cbd007a562ab44c2bf3c779d9680bd
imfulee <imfulee@gmail.com>
2020-12-10 23:32:51 +0800

    wip: fixed more bugs

--- use_reasoner.py
+++ use_reasoner.py
@@ -11,11 +11,14 @@ demo_class = 'Demo'
 
 debug = True
 
-ape_command = lambda filename: [ape, '-file', filename, '-solo', 'owlxml']
-owl_to_ace_command = lambda filename: [owl_to_ace, '-xml', filename]
-
+# Get the commands formating right for the right operating system
 plat = platform.system()
-exe_tail = '.exe'
+exe_head, exe_tail = '', '.exe'
+if plat != "Windows":
+	exe_head = './'
+
+ape_command = lambda filename: [f'{exe_head}{ape}{exe_tail}', '-file', filename, '-solo', 'owlxml', '-guess']
+owl_to_ace_command = lambda filename: [f'{exe_head}{owl_to_ace}{exe_tail}', '-xml', filename]
 
 if not os.path.exists(reasoner):
 	print("Cannot find reasoner.")
@@ -26,14 +29,34 @@ if not os.path.exists(f'{ape}{exe_tail}'):
 if not os.path.exists(f'{owl_to_ace}{exe_tail}'):
 	print("Cannot find owl_to_ace")
 
+# compiling Demo.class
 if not os.path.exists(f'{demo_class}.class'):
 	subprocess.run(
 		['javac',
 		 '-cp',
 		 reasoner,
-		 f'{demo_class}.java'], shell = True)
+		 f'{demo_class}.java'])
 
-def run(storypath, querypath):
+def ace_to_owl(string: str, directory: str) -> str:
+	"""Write the ACE string into a OWL file (XML format)
+
+	- This is a function acting as a proof of concept rather than actually being used
+
+	Args:
+		string (str): the ACE string that needs to be translated
+		directory (str): the directory in which the file is saved as (*.owl)
+	"""
+	string.replace('\n', ' ')
+	with open(f'./capture/{directory}.owl', 'w') as file:
+		s = subprocess.run([f'{exe_head}{ape}{exe_tail}', 
+							'-text', f'{string}', 
+							'-solo', 'owlxml', 
+							'-guess'],
+						   capture_output=True)
+		file.write(str(s.stdout.decode('utf-8')))
+	file.close()
+
+def run_deprecated(storypath, querypath):
 	with open(f'{storypath}.owl', 'wb') as file:
 		s = subprocess.run(ape_command(filename = storypath), capture_output = True)
 		file.write(s.stdout)
@@ -58,3 +81,34 @@ def run(storypath, querypath):
 		return 'False'
 	else:
 		return result.stdout.decode()
+
+def run(storypath: str, querypath: str):
+	# java -cp seperator
+	if plat == 'Windows': 
+		seperator = ';'
+	else:
+		seperator = ':'
+	
+	result = subprocess.run(
+		['java',
+		 '-cp',
+		 f'.{seperator}{reasoner}',
+		 demo_class,
+		 'e',
+		 os.path.abspath(f'./capture/{storypath}.owl'),
+		 os.path.abspath(f'./capture/{querypath}.owl')],
+		 capture_output = True, 
+		 shell = True)
+	
+	print("This", result.stdout)
+	
+	if not debug:
+		os.remove(f'{storypath}.owl')
+		os.remove(f'{querypath}.owl')
+
+	if b'true' in result.stdout:
+		return 'True'
+	elif b'false' in result.stdout:
+		return 'False'
+	else:
+		return result.stdout.decode('utf-8')
